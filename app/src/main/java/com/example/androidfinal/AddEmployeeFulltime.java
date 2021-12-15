f72a9a0227f7e10a0b8a9104488ceb24e3137b8f
Alejandro <60989886+ed3642@users.noreply.github.com>
2021-12-14 22:54:49 -0500

    updated adding employees

--- app/src/main/java/com/example/androidfinal/AddEmployeeFulltime.java
+++ app/src/main/java/com/example/androidfinal/AddEmployeeFulltime.java
@@ -3,6 +3,8 @@ package com.example.androidfinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class AddEmployeeFulltime extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class AddEmployeeFulltime extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_employee_fulltime);
+    }
+
+    public void handleSubmitAddFullTime(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT);
+
+        toast.show();
     }
 }
--- app/src/main/java/com/example/androidfinal/AddEmployeeParttime.java
+++ app/src/main/java/com/example/androidfinal/AddEmployeeParttime.java
@@ -3,6 +3,8 @@ package com.example.androidfinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class AddEmployeeParttime extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class AddEmployeeParttime extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_employee_parttime);
+    }
+
+    public void handleSubmitAddPartTime(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT);
+
+        toast.show();
     }
 }
